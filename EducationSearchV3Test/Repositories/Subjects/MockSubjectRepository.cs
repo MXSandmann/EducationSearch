ee779e6e781aaff7bd3f9bb1a4a0b962d01ea838
mxchainsaw <quit>
2022-11-27 16:18:05 +0100

    Add first unit tests

---
+++ EducationSearchV3Test/Repositories/Subjects/MockSubjectRepository.cs
@@ -0,0 +1,43 @@
+﻿using EducationSearchV3.Models.Dtos.Responses;
+using EducationSearchV3.Repositories;
+using Moq;
+
+namespace EducationSearchV3Test.Repositories.Subjects
+{
+    internal static class MockSubjectRepository
+    {
+        public static Mock<ISubjectRepository> GetInstance()
+        {
+            var subjests = GetAllSubjects();
+            var mockRepo = new Mock<ISubjectRepository>();
+            mockRepo.Setup(x => x.GetAll())
+                .ReturnsAsync(() =>
+                {
+                    return subjests;
+                });
+            return mockRepo;
+        }
+
+        private static List<GetSubjectDto> GetAllSubjects() => new()
+        {
+            new GetSubjectDto
+            {
+                Id = 1,
+                Name = "Test1",
+                Programs = null
+            },
+            new GetSubjectDto
+            {
+                Id = 2,
+                Name = "Test2",
+                Programs = null
+            },
+            new GetSubjectDto
+            {
+                Id = 3,
+                Name = "Test3",
+                Programs = null
+            }
+        };            
+    }
+}
